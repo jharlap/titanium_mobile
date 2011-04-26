tidev/titanium-sdk

enable custom TitleView on windows

android/titanium/src/org/appcelerator/titanium/TiC.java
@@ -129,6 +129,7 @@ public class TiC
 	public static final String PROPERTY_CONTENT_VIEW = "contentView";
 	public static final String PROPERTY_COUNTRY = "country";
 	public static final String PROPERTY_COUNTRY_CODE = "countryCode";
+	public static final String PROPERTY_CUSTOM_TITLE_VIEW = "customTitle";
 	public static final String PROPERTY_DATA = "data";
 	public static final String PROPERTY_DEFAULT_IMAGE = "defaultImage";
 	public static final String PROPERTY_DEFAULTS = "defaults";
@@ -252,6 +253,7 @@ public class TiC
 	public static final String PROPERTY_TIME = "time";
 	public static final String PROPERTY_TIMESTAMP = "timestamp";
 	public static final String PROPERTY_TITLE = "title";
+	public static final String PROPERTY_TITLE_CONTROL = "titleControl";
 	public static final String PROPERTY_TITLEID = "titleid";
 	public static final String PROPERTY_TOP = "top";
 	public static final String PROPERTY_TOUCH_ENABLED = "touchEnabled";

jharlap/titanium_mobile

android/titanium/src/org/appcelerator/titanium/TiC.java
@@ -190,7 +190,7 @@ public class TiC
 	public static final String PROPERTY_MAGNETIC_HEADING = "magneticHeading";
 	public static final String PROPERTY_MAP_TYPE = "mapType";
 	public static final String PROPERTY_MENU = EVENT_PROPERTY_MENU;
-	public static final String PROPERTY_MAX_LINES = "maxLines"; 
+	public static final String PROPERTY_MAX_LINES = "maxLines";
 	public static final String PROPERTY_MESSAGE = "message";
 	public static final String PROPERTY_MIN_ROW_HEIGHT = "minRowHeight";
 	public static final String PROPERTY_MODAL = "modal";